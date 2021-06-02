73777b740a3bc07de9b2a6f7ffa46d6764602ae0
Yachika Malik <84844709+Yachikam@users.noreply.github.com>
2021-06-02 17:00:01 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,14 @@
+#TASK 1:
+
+#Find the count of each character
+
+#Check if all the counts are same or at most one character is one extra than
+#all the others, Its MYSTRING else NOT MYSTRING.
+
+a = input('Enter any characters: ')
+b = {i:a.count(i) for i in a}
+print(b)
+
+c = list(b.values())
+
+print("MYSTRING") if (max(c)-min(c)<=1 and c.count(max(c))==1) else print("NOT MYSTRING")
